9af29473e9770e5463fefec1cd864fb98bdb832b
tornikekhokhiashvili <tornikexoxiashvili@gmail.com>
2025-04-18 18:37:50 +0400

    Add styling and structure for unit conversion application

--- index.js
+++ index.js
@@ -3,3 +3,28 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const input = document.getElementById("header-input");
+const lengthCard = document.getElementById("length");
+const volumeCard = document.getElementById("volume");
+const massCard = document.getElementById("mass");
+
+function converter() {
+  if (input.value != "") {
+    lengthCard.innerHTML = `${input.value} meters = ${(
+      input.value * 3.281
+    ).toFixed(3)} feet | ${input.value} feet = ${(input.value / 3.281).toFixed(
+      3
+    )} meters`;
+    volumeCard.innerHTML = `${input.value} liters = ${(
+      input.value * 0.264
+    ).toFixed(3)} gallons | ${input.value} gallons = ${(
+      input.value / 0.263
+    ).toFixed(3)} liters `;
+    massCard.innerHTML = `${input.value} kilos = ${(
+      input.value * 2.204
+    ).toFixed(3)} pounds | ${input.value} pounds = ${(
+      input.value / 2.204
+    ).toFixed(3)} kilos`;
+  }
+}
